c2e233407a5a0daddb4cf53e29159260b1fa57bf
lukemesiti <lukemesiti@gmail.com>
2016-02-01 21:52:03 +1100

    write test to qualify getColorGradient arguments

--- src/js/color-palette.js
+++ src/js/color-palette.js
@@ -1,9 +1,24 @@
 'use strict';
 
+Number.isInteger = Number.isInteger || function(value) {
+  return typeof value === "number" &&
+    isFinite(value) &&
+    Math.floor(value) === value;
+};
+
 (function(exports){
   // var colorPalette = {};
 
-  exports.getColorGradient = function() {
+  exports.getColorGradient = function(hex, length) {
+    if (hex == null || length == null) {
+      return [];
+    }
+    if(!/^#([0-9a-f]{3}){1,2}$/.test(hex)) {
+      return [];
+    }
+    if(!Number.isInteger(length) || length <= 1 || length > 5) {
+      return [];
+    }
   };
 
 })(this.colorPalette = {});
--- test/spec/spec-colorPalette.js
+++ test/spec/spec-colorPalette.js
@@ -3,7 +3,44 @@ describe("color-palette", function () {
     expect(colorPalette).toBeDefined();
   });
 
-  it('should expose a public function', function () {
+  it("should expose a public function", function () {
     expect(colorPalette.getColorGradient).toEqual(jasmine.any(Function));
+  });
+
+  describe("when calling getColorGradient", function () {
+    it("should return an empty array if no arguments supplied", function () {
+      var colorGradient = colorPalette.getColorGradient()
+      expect(colorGradient).toEqual([]);
+    });
+    it("should return an empty array if only one argument supplied", function () {
+      var colorGradient = colorPalette.getColorGradient(1);
+      expect(colorGradient).toEqual([]);
+    });
+    it("should return an empty array if first argument is not a hex value", function () {
+      var colorGradient1 = colorPalette.getColorGradient("asdf", 1);
+      var colorGradient2 = colorPalette.getColorGradient("#123erq", 1);
+      var colorGradient3 = colorPalette.getColorGradient("123", 1);
+      var colorGradient4 = colorPalette.getColorGradient("#asdf", 1);
+      expect(colorGradient1).toEqual([]);
+      expect(colorGradient2).toEqual([]);
+      expect(colorGradient3).toEqual([]);
+      expect(colorGradient4).toEqual([]);
+    });
+    it("should return an empty array if the second argument is not a number between 1 and 5", function () {
+      var colorGradient1 = colorPalette.getColorGradient("#fff", 0);
+      var colorGradient2 = colorPalette.getColorGradient("#fff", -1);
+      var colorGradient3 = colorPalette.getColorGradient("#fff", 6);
+      var colorGradient4 = colorPalette.getColorGradient("#fff", "one");
+      var colorGradient5 = colorPalette.getColorGradient("#fff", null);
+      var colorGradient6 = colorPalette.getColorGradient("#fff", undefined);
+      var colorGradient7 = colorPalette.getColorGradient("#fff", NaN);
+      expect(colorGradient1).toEqual([]);
+      expect(colorGradient2).toEqual([]);
+      expect(colorGradient3).toEqual([]);
+      expect(colorGradient4).toEqual([]);
+      expect(colorGradient5).toEqual([]);
+      expect(colorGradient6).toEqual([]);
+      expect(colorGradient7).toEqual([]);
+    });
   });
 });
