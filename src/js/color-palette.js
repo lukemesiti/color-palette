0b099a93d6c1152d480846510005820490944c86
lukemesiti <lukemesiti@gmail.com>
2016-02-24 21:15:29 +1100

    allow unlimited gradient options modified tests

--- src/js/color-palette.js
+++ src/js/color-palette.js
@@ -7,6 +7,8 @@ Number.isInteger = Number.isInteger || function(value) {
 };
 
 (function(exports){
+  // todo: get analogous colors
+  // todo: get complimentary colors
 
   exports.getColorGradient = function(hex, length) {
     if (hex == null || length == null) {
@@ -19,87 +21,30 @@ Number.isInteger = Number.isInteger || function(value) {
     if(!Number.isInteger(length)) {
       return [];
     }
-    if(length < 1 || length > 5) {
+    if(length < 1) {
       return [];
     }
 
     var colorGradient = [];
-    // for(var i = 0; i < length; i++) {
-    //   colorGradient.push(hex);
-    // }
-
     var rgb = hexToRgb(hex);
     var hsv = rgbToHsv(rgb.r, rgb.g, rgb.b);
 
-    var variations = getColorVariations(hsv.s, hsv.v);
+    // tints
+    for(var i = 1; i <= length/2; i++) {
+      rgb = hsvToRgb(hsv.h, 1/(length/2)*i, 1);
+      colorGradient.push(rgbToHex(rgb.red, rgb.green, rgb.blue));
+    }
 
-    rgb = hsvToRgb(hsv.h, variations[0][0], variations[0][1]);
-    var firstVariation = rgbToHex(rgb.red, rgb.green, rgb.blue);
-
-    rgb = hsvToRgb(hsv.h, variations[1][0], variations[1][1]);
-    var secondVariation = rgbToHex(rgb.red, rgb.green, rgb.blue);
-
-    colorGradient.push(hex);
-    colorGradient.push(firstVariation);
-    colorGradient.push(secondVariation);
-
+    // shades
+    for(var i = length/2; i > 0; i--) {
+      rgb = hsvToRgb(hsv.h, 1, 1/(length/2)*i);
+      colorGradient.push(rgbToHex(rgb.red, rgb.green, rgb.blue));
+    }
     return colorGradient;
   };
 
-  function getColorVariations(saturation, v) {
-     var middleOf = {
-         areaOne: [0.5, 0.9],
-         areaTwo: [0.7, 0.9],
-         areaThree: [0.9, 0.9],
-         areaFour: [0.5, 0.7],
-         areaFive: [0.7, 0.7],
-         areaSix: [0.9, 0.7],
-         areaSeven: [0.5, 0.5],
-         areaEight: [0.7, 0.5],
-         areaNine: [0.7, 0.5]
-     }
-
-     // area 1
-     if (saturation >= 0.40 && saturation < 0.60 && v >= 0.80) {
-         return [middleOf.areaThree, middleOf.areaNine];
-     }
-     // area 2
-     else if (saturation >= 0.60 && saturation < 0.80 && v >= 0.80) {
-         return [middleOf.areaSeven, middleOf.areaNine];
-     }
-     // area 3
-     else if (saturation >= 0.80 && value >= 0.80) {
-         return [middleOf.areaOne, middleOf.areaNine];
-     }
-     // area 4
-     else if (saturation >= 0.40 && saturation < 0.60 && v >= 0.60 && v < 0.80) {
-         return [middleOf.areaThree, middleOf.areaNine];
-     }
-     // area 5
-     else if (saturation >= 0.60 && saturation < 0.80 && v >= 0.60 && v < 0.80) {
-         return [middleOf.areaOne, middleOf.areaNine];
-     }
-     // area 6
-     else if (saturation >= 0.80 && v >= 0.60 && v < 0.80) {
-         return [middleOf.areaOne, middleOf.areaSeven];
-     }
-     // area 7
-     else if (saturation >= 0.40 && saturation < 0.60 && v >= 0.40 && v < 0.60) {
-         return [middleOf.areaOne, middleOf.areaNine];
-     }
-     // area 8
-     else if (saturation >= 0.60 && saturation < 0.80 && v >= 0.40 && v < 0.60) {
-         return [middleOf.areaOne, middleOf.areaThree];
-     }
-     // area 9
-     else if (saturation >= 0.80 && v >= 0.40 && v < 0.60) {
-         return [middleOf.areaThree, middleOf.areaSeven];
-     }
-     return [middleOf.areaTwo, middleOf.areaSix];
-  }
-
   function hexToRgb(hex) {
-    var hex = hex.substring(1);    // strip #
+    var hex = hex.substring(1);
     if(hex.length === 3) {
       hex = hex.concat(hex);
     }
@@ -123,82 +68,47 @@ Number.isInteger = Number.isInteger || function(value) {
     return '#' + r + g + b;
   }
 
-  function rgbToHsv(red, green, blue) {
-    red = red / 255;
-    green = green / 255;
-    blue = blue / 255;
+  function rgbToHsv(r, g, b) {
+    r = r / 255, g = g / 255, b = b / 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, v = max;
 
-    var max = Math.max(red, green, blue);
-    var min = Math.max(red, green, blue);
-    var delta = max - min;
-    var saturation = max == 0 ? 0 : delta / max;
-    var hue = 0;
+    var d = max - min;
+    s = max == 0 ? 0 : d / max;
 
-    if (delta) {
-      switch (max) {
-        case red:
-          hue = ((green - blue) / delta);
-          break;
-        case green:
-          hue = 2 + (blue - red) / delta;
-          break;
-        case blue:
-          hue = 4 + (red - green) / delta;
-          break;
-      }
-		}
-
-		hue = 60 * hue | 0;
-		if (hue < 0) hue += 360;
-		saturation = (saturation * 100) | 0;
-		var value = (max * 100) | 0;
-
-    return {
-      h: hue,
-      s: saturation,
-      v: value
+    if (max == min) {
+        h = 0; // achromatic
+    } else {
+        switch (max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
     }
+
+    return {h: h, s: s, v: v};
   }
 
-  // function hsvToRgb(h, s, v) {
-  //     var r, g, b;
-  //
-  //     var i = Math.floor(h * 6);
-  //     var f = h * 6 - i;
-  //     var p = v * (1 - s);
-  //     var q = v * (1 - f * s);
-  //     var t = v * (1 - (1 - f) * s);
-  //
-  //     switch (i % 6) {
-  //         case 0: r = v, g = t, b = p; break;
-  //         case 1: r = q, g = v, b = p; break;
-  //         case 2: r = p, g = v, b = t; break;
-  //         case 3: r = p, g = q, b = v; break;
-  //         case 4: r = t, g = p, b = v; break;
-  //         case 5: r = v, g = p, b = q; break;
-  //     }
-  //
-  //     return { red: r * 255, green: g * 255, blue: b * 255};
-  // }
+  function hsvToRgb(h, s, v) {
+      var r, g, b;
 
-  function hsvToRgb(hue, saturation, value) {
-    saturation = saturation / 100;
-    value = value / 100;
-    var chroma = saturation * value;
-    var h = hue / 60;
-    var x = chroma * (1 - Math.abs(h % 2 - 1));
-    var m = value - chroma;
+      var i = Math.floor(h * 6);
+      var f = h * 6 - i;
+      var p = v * (1 - s);
+      var q = v * (1 - f * s);
+      var t = v * (1 - (1 - f) * s);
 
-    chroma = (chroma + m) * 255 | 0;
-    x = (x + m) * 255 | 0;
-    m = m * 255 | 0;
+      switch (i % 6) {
+          case 0: r = v, g = t, b = p; break;
+          case 1: r = q, g = v, b = p; break;
+          case 2: r = p, g = v, b = t; break;
+          case 3: r = p, g = q, b = v; break;
+          case 4: r = t, g = p, b = v; break;
+          case 5: r = v, g = p, b = q; break;
+      }
 
-    if (h >= 0 && h < 1) {	return { red: chroma, green: x, blue: m}; }
-		if (h >= 1 && h < 2) {	return { red: x, green: chroma, blue: m}; }
-		if (h >= 2 && h < 3) {	return { red: m, green: chroma, blue: x}; }
-		if (h >= 3 && h < 4) {	return { red: m, green: x, blue: chroma}; }
-		if (h >= 4 && h < 5) {	return { red: x, green: m, blue: chroma}; }
-		if (h >= 5 && h < 6) {	return { red: chroma, green: m, blue: x}; }
+      return { red: r * 255, green: g * 255, blue: b * 255};
   }
 
 })(this.colorPalette = {});
--- test/spec/spec-colorPalette.js
+++ test/spec/spec-colorPalette.js
@@ -26,30 +26,28 @@ describe("color-palette", function () {
       expect(colorGradient3).toEqual([]);
       expect(colorGradient4).toEqual([]);
     });
-    it("should return an empty array if the second argument is not a number between 1 and 5", function () {
+    it("should return an empty array if the second argument is not greater than 1", function () {
       var colorGradient1 = colorPalette.getColorGradient("#fff", 0);
       var colorGradient2 = colorPalette.getColorGradient("#fff", -1);
-      var colorGradient3 = colorPalette.getColorGradient("#fff", 6);
       var colorGradient4 = colorPalette.getColorGradient("#fff", "one");
       var colorGradient5 = colorPalette.getColorGradient("#fff", null);
       var colorGradient6 = colorPalette.getColorGradient("#fff", undefined);
       var colorGradient7 = colorPalette.getColorGradient("#fff", NaN);
       expect(colorGradient1).toEqual([]);
       expect(colorGradient2).toEqual([]);
-      expect(colorGradient3).toEqual([]);
       expect(colorGradient4).toEqual([]);
       expect(colorGradient5).toEqual([]);
       expect(colorGradient6).toEqual([]);
       expect(colorGradient7).toEqual([]);
     });
-    // it("should return an array the size of the length argument if all arguments are valid", function () {
-    //   var colorGradient1 = colorPalette.getColorGradient("#fff", 1);
-    //   var colorGradient2 = colorPalette.getColorGradient("#fff", 2);
-    //   var colorGradient5 = colorPalette.getColorGradient("#fff", 5);
-    //   expect(colorGradient1.length).toEqual(1);
-    //   expect(colorGradient2.length).toEqual(2);
-    //   expect(colorGradient5.length).toEqual(5);
-    // });
+    it("should return an array the size of the length argument if all arguments are valid", function () {
+      var colorGradient1 = colorPalette.getColorGradient("#fff", 1);
+      var colorGradient2 = colorPalette.getColorGradient("#fff", 2);
+      var colorGradient5 = colorPalette.getColorGradient("#fff", 5);
+      expect(colorGradient1.length).toEqual(1);
+      expect(colorGradient2.length).toEqual(2);
+      expect(colorGradient5.length).toEqual(5);
+    });
     it("should return an array of shades of the provided hex value", function () {
       var colorGradient1 = colorPalette.getColorGradient("")
     });
